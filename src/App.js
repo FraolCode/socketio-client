141dc061c9320de324c75c2ce55f5f86bc650864
FraolCode <fraol.code@gmail.com>
2023-12-04 17:25:48 +0300

    client

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import io from 'socket.io-client';
+import { useEffect } from 'react';
+
+const socket = io.connect("http://localhost:3001");
+
 
 function App() {
+
+  const sendmessage = () =>{
+    socket.emit("send_message", {message : "hello"} )
+  };
+ 
+  useEffect(() => {
+    socket.on("received_message",(message)=> {
+      alert(message.message);
+    });
+  }, [socket]);
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" placeholder="message" id="message" name="message" ></input>
+      <button onClick={sendmessage} type="button"> Send message </button>
     </div>
   );
 }
